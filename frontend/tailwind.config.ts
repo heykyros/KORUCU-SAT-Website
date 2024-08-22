1821685335b22b2011c91a66031015c4b39346db
Eesa Abdulraouf <eesaaraouf@gmail.com>
2024-08-22 11:18:27 -0400

    Initial commit, designing stage

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      'darkerpurple': '#8757BA',
+      'lighterpurple': '#C979FF',
+      'platinum': '#E2E2E2',
+      'spacecadet': '#111133',
+      'night': '#131112',
+    },
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
